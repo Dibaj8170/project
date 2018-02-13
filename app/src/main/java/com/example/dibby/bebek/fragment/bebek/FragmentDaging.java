9cd46badecde1dd84e47e10231b0c06059bf0eea
dibby <dibaj8170@students.amikom.ac.id>
2018-02-13 20:55:59 +0700

    add Fragment

---
+++ app/src/main/java/com/example/dibby/bebek/fragment/bebek/FragmentDaging.java
@@ -0,0 +1,8 @@
+package com.example.dibby.bebek.fragment.bebek;
+
+/**
+ * Created by dibby on 13/02/2018.
+ */
+
+public class FragmentDaging {
+}
---
+++ app/src/main/java/com/example/dibby/bebek/fragment/caraBertenak/FragmentNewBorn.java
@@ -0,0 +1,8 @@
+package com.example.dibby.bebek.fragment.caraBertenak;
+
+/**
+ * Created by dibby on 13/02/2018.
+ */
+
+public class FragmentNewBorm {
+}
