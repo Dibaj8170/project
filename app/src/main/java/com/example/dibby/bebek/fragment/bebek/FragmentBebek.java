761af84be6b638653b687dfac49f8d475b9224c7
dibby <dibaj8170@students.amikom.ac.id>
2018-02-17 11:03:34 +0700

    add eventhandler at bottom menu bebek to fragment

--- app/src/main/java/com/example/dibby/bebek/fragment/bebek/FragmentBebek.java
+++ app/src/main/java/com/example/dibby/bebek/fragment/bebek/FragmentBebek.java
@@ -1,8 +1,56 @@
 package com.example.dibby.bebek.fragment.bebek;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dibby.bebek.R;
+
 /**
  * Created by dibby on 13/02/2018.
  */
 
-public class FragmentBebek {
+public class FragmentBebek  extends Fragment{
+
+    private OnFragmentInteractionListener listener;
+
+    public static FragmentBebek newInstance(){
+        return new FragmentBebek();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.fragment_bebek, container, false
+        );
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnFragmentInteractionListener){
+            listener = (FragmentBebek.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+
 }
--- app/src/main/java/com/example/dibby/bebek/fragment/bebek/FragmentDaging.java
+++ app/src/main/java/com/example/dibby/bebek/fragment/bebek/FragmentDaging.java
@@ -1,8 +1,56 @@
 package com.example.dibby.bebek.fragment.bebek;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dibby.bebek.R;
+
 /**
  * Created by dibby on 13/02/2018.
  */
 
-public class FragmentDaging {
+public class FragmentDaging extends Fragment {
+
+    private OnFragmentInteractionListener listener;
+
+    public static FragmentDaging newInstance(){
+        return new FragmentDaging();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.fragment_daging, container, false
+        );
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnFragmentInteractionListener){
+            listener = (FragmentDaging.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+
 }
