15452ce8bcd93c4d119b93dbf3a3c7e731a1a4fb
dibby <dibaj8170@students.amikom.ac.id>
2018-02-17 11:38:24 +0700

    add eventhandler at bottom menu cara bertenak to fragment

--- app/src/main/java/com/example/dibby/bebek/fragment/caraBertenak/FragmentPedaging.java
+++ app/src/main/java/com/example/dibby/bebek/fragment/caraBertenak/FragmentPedaging.java
@@ -1,8 +1,56 @@
 package com.example.dibby.bebek.fragment.caraBertenak;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dibby.bebek.R;
+
 /**
  * Created by dibby on 13/02/2018.
  */
 
-public class FragmentPedaging {
+public class FragmentPedaging extends Fragment {
+
+    private OnFragmentInteractionListener listener;
+
+    public static FragmentPedaging newInstance(){
+        return new FragmentPedaging();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.fragment_pedaging, container, false
+        );
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnFragmentInteractionListener){
+            listener = (FragmentPedaging.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+
 }
--- app/src/main/java/com/example/dibby/bebek/view/CaraBeternak.java
+++ app/src/main/java/com/example/dibby/bebek/view/CaraBeternak.java
@@ -1,15 +1,64 @@
 package com.example.dibby.bebek.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.dibby.bebek.R;
+import com.example.dibby.bebek.fragment.caraBertenak.FragmentLingkungan;
+import com.example.dibby.bebek.fragment.caraBertenak.FragmentNewBorn;
+import com.example.dibby.bebek.fragment.caraBertenak.FragmentPedaging;
+import com.example.dibby.bebek.fragment.caraBertenak.FragmentPetelur;
 
-public class CaraBeternak extends AppCompatActivity {
+public class CaraBeternak extends AppCompatActivity implements
+        FragmentPedaging.OnFragmentInteractionListener,
+        FragmentNewBorn.OnFragmentInteractionListener,
+        FragmentPetelur.OnFragmentInteractionListener,
+        FragmentLingkungan.OnFragmentInteractionListener,
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomMenuCara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cara_beternak);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.cara_frame_layout, FragmentNewBorn.newInstance());
+        fragmentTransaction.commit();
+
+        bottomMenuCara = findViewById(R.id.cara_bottom_menu);
+        bottomMenuCara.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.menu_new_born :
+                fragment = FragmentNewBorn.newInstance();
+                break;
+            case R.id.menu_petelur :
+                fragment = FragmentPetelur.newInstance();
+                break;
+            case R.id.menu_pedaging :
+                fragment = FragmentPedaging.newInstance();
+                break;
+            case R.id.menu_lingkungan :
+                fragment = FragmentLingkungan.newInstance();
+                break;
+        }
+
+        if (fragment != null){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.cara_frame_layout, fragment);
+            fragmentTransaction.commit();
+        }
+        return true;
     }
 }
